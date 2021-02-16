e17944776303435b922fd4bbeb356d15a3e2742b
miguepoloc <miguepoloc@gmail.com>
2021-02-16 15:40:00 -0500

    Primer commit

---
+++ pages/encabezado.php
@@ -0,0 +1,22 @@
+<div class="py-2 bg-primary">
+    <div class="container">
+        <div class="row no-gutters d-flex align-items-start align-items-center px-3 px-md-0">
+            <div class="col-lg-12 d-block">
+                <div class="row d-flex">
+                    <div class="col-md-5 pr-4 d-flex topper align-items-center">
+                        <div class="icon bg-fifth mr-2 d-flex justify-content-center align-items-center"><span class="icon-map"></span></div>
+                        <span class="text">Calle 29 N°31-480, Santa Marta, Magdalena</span>
+                    </div>
+                    <div class="col-md pr-4 d-flex topper align-items-center">
+                        <div class="icon bg-secondary mr-2 d-flex justify-content-center align-items-center"><span class="icon-paper-plane"></span></div>
+                        <span class="text">inemsb.institucional@gmail.com</span>
+                    </div>
+                    <div class="col-md pr-4 d-flex topper align-items-center">
+                        <div class="icon bg-tertiary mr-2 d-flex justify-content-center align-items-center"><span class="icon-phone2"></span></div>
+                        <span class="text">+57 312 263 8953</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
