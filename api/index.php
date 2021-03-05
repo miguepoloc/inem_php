3a0913bdf7dc4be9092699ff518e1fd430fe1f92
miguepoloc <miguepoloc@gmail.com>
2021-03-04 22:15:09 -0500

    Primeros inicios con bdd

---
+++ api/index.php
@@ -0,0 +1,6 @@
+<?php
+    include_once 'apipeliculas.php';
+
+    $api = new ApiPeliculas();
+
+    $api->getAll();
---
+++ api/test/pdoconfig.php
@@ -0,0 +1,5 @@
+<?php
+    $host = 'localhost';
+    $dbname = 'prueba';
+    $username = 'root';
+    $password = '';
